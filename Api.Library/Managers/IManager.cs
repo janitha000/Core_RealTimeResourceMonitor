bf52c801783fffb6a4d9f68d4e77ed81e39806cb
Janitha Tennakoon <janitha000@gmail.com>
2017-10-20 08:12:19 +0530

    Code beautification

--- Api.Library/Managers/IManager.cs
+++ Api.Library/Managers/IManager.cs
@@ -2,9 +2,8 @@ using System.Collections.Generic;
 
 public interface IManager<T>
 {
-    T Get(string param);
-    void Add(T data);  
     IEnumerable<T> GetAll();
-
+    T Get(string param);
+    void Add(T data);
     bool Exists(string param);
 }
--- Api.Tests/UserManagerTests.cs
+++ Api.Tests/UserManagerTests.cs
@@ -11,14 +11,8 @@ public class UserManagerTests
 
     public UserManagerTests()
     {
-
-
         _mockRepository = new Mock<IRepository<User>>();
-
-        _mockRepository.Setup(repo => repo.Get(mockUser => mockUser.FirstName == "Janitha")).Returns(new User() { FirstName = "Janitha", LastName = "Tennakoon", Age = 26 });
-
         _userManager = new UsersManager(_mockRepository.Object);
-
     }
 
     [TestMethod]
@@ -42,54 +36,19 @@ public class UserManagerTests
         var expectedUser = new User() { FirstName = "Janitha", LastName = "Tennakoon", Age = 27 };
 
         var user = _userManager.Get(name);
-
     }
 
     [TestMethod]
     public void Should_add_one_user()
     {
-        string name = "Janitha";
         var user = new User() { FirstName = "Vindya", LastName = "Hettige", Age = 27 };
-
-       _userManager.Add(user);
-
+        _userManager.Add(user);
     }
 
     [TestMethod]
-    public void Should_Check_Exists(){
+    public void Should_Check_Exists()
+    {
         string name = "Shanika";
         bool result = _userManager.Exists(name);
     }
-
-
-
-
-
-    // [TestMethod]
-    // public void IsUserNameNullTest()
-    // {
-    //     User user = new User();
-    //     Assert.IsTrue(_userManager.IsUserNameNull(user), "IsNull should be true when the user name is Null");
-    // }
-
-    // [TestMethod]
-    // public void TestGetUsers()
-    // {
-    //     User user = _userManager.Get("Janitha");
-    //     Assert.IsFalse(_userManager.IsUserNameNull(user), "A user should be returned from the database");
-    // }
-
-    // [TestMethod]
-    // public void AddTestUser()
-    // {
-    //     User user = new User() { FirstName = "Vindya", LastName = "Hettige", Age = 27 };
-    //     _userManager.AddUser(user);
-
-    //     User addedUser = _userManager.GetUser("Vindya");
-    //     Assert.IsNotNull(addedUser, "Adding users failed");
-
-
-    // }
-
-
 }
