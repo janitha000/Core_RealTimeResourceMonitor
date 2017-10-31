83c58c77f5724488da9a8ba61f2666e6b412e400
Janitha Tennakoon <janitha000@gmail.com>
2017-10-31 08:10:17 +0530

    New code

--- Api.Tests/UnitTests/UserControllerTests.cs
+++ Api.Tests/UnitTests/UserControllerTests.cs
@@ -21,9 +21,10 @@ public class UserControllerTests
             new User { FirstName = "Janitha", LastName = "Tennakoon"},
             new User {FirstName = "Vindya", LastName = "Hettige"}
         };
-        moqUserManager.Setup(x => x.GetAll()).Returns(expectedUsers);
+        moqUserManager.Setup(x => x.GetAll()).Returns(expectedUsers).Verifiable();
 
         var result = userController.GetAllAsync();
+        moqUserManager.Verify(x => x.GetAll(), Times.Once);
         //Assert.AreSame(expectedUsers, result.Result, "Should return users");
     }
 
